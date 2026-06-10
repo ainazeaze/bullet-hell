57cf30e71b06c092383beaef0d41b2263b7209be
ainazeaze <aratefiarivony8@gmail.com>
2026-06-10 13:47:31 +0200

    feat : enemy damaging player

--- Enemy.gd
+++ Enemy.gd
@@ -4,6 +4,8 @@ const SPEED = 100.0
 
 var player : Node2D = null
 
+@onready var area : Area2D = $Area2D
+
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 	print("Player found" , player)
@@ -15,3 +17,9 @@ func _physics_process(delta: float) -> void:
 	var direction = (player.global_position - global_position).normalized()
 	velocity = direction * SPEED
 	move_and_slide()
+	hit_player()
+	
+func hit_player() -> void:
+	for body in area.get_overlapping_bodies():
+		if body.is_in_group("player"):
+			body.take_damage(10)
--- Player.gd
+++ Player.gd
@@ -1,8 +1,27 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+var hp = 100
+var invincible = false
+
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * SPEED
 	move_and_slide()
+
+func take_damage(amount : int) -> void:
+	if invincible:
+		return
+	hp -= amount
+	invincible = true
+	print("Player HP: ", hp)
+	if hp <= 0:
+		queue_free()
+		return
+	await get_tree().create_timer(1.0).timeout
+	invincible = false
+
+func _on_invicibility_timer() -> void :
+	invincible = false
+	
